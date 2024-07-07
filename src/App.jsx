5078918c202f9a9e075cfacce654d6705cdb5cab
Mahmoudbaky <ma7mouudbaky@gmail.com>
2024-07-07 23:47:23 +0300

    v 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 function App() {
+  document.title = "Mahmoud's Portfolio";
   return (
     <div>
       <Navbar />
